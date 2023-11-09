4e9a96c6a6ac9f44d39068f3ba8378d67e8b5b81
cfandrews <cfandrews@outlook.com>
2023-11-08 22:42:59 -0500

    Initial commit

---
+++ src/pythontemplate/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2023 Charles Andrews
+""".. include:: ../../README.md"""  # noqa: D415
+from __future__ import annotations
+
+from typing import Final
+
+from .main import main
+
+__all__: Final[list[str]] = ["main"]
+__docformat__: Final[str] = "google"
