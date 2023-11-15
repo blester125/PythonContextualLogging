6624be07033fc0cb4450b638cca549698f4bdf0f
Charles Andrews <cfandrews@outlook.com>
2023-11-14 19:23:29 -0500

    Update ruff

--- src/contextuallogging/__init__.py
+++ src/contextuallogging/__init__.py
@@ -1,5 +1,17 @@
 # Copyright 2023 Charles Andrews
-""".. include:: ../../README.md"""  # noqa: D415
+# fmt: off
+"""[![Build Status](https://github.com/cfandrews/PythonContextualLogging/actions/workflows/build.yml/badge.svg)](https://github.com/cfandrews/PythonContextualLogging/actions)
+[![Documentation Status](https://github.com/cfandrews/PythonContextualLogging/actions/workflows/documentation.yml/badge.svg)](https://github.com/cfandrews/PythonContextualLogging/actions)
+[![Checked with mypy](https://www.mypy-lang.org/static/mypy_badge.svg)](https://mypy-lang.org/)
+[![Linting: Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/charliermarsh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+
+Pure-Python semantic logging library with thread-local context.
+
+[Source](https://github.com/cfandrews/PythonContextualLogging/)  
+[Documentation](https://cfandrews.github.io/PythonContextualLogging/contextuallogging.html)
+"""  # noqa: W291, D205, D415
+# fmt: on
+
 from __future__ import annotations
 
 from typing import Final
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 # Copyright 2023 Charles Andrews
 """Unit tests for contextuallogging.main."""
+
 from contextuallogging import main
 
 
